7f838ceb31e23cc9122f9fce2abc1e507024af91
newMdd <3011878963@qq.com>
2023-01-09 20:05:33 +0800

    feat:增加问题回答

--- src/cli-commands/init.js
+++ src/cli-commands/init.js
@@ -4,6 +4,7 @@ import { copyDir } from '../utils/copyDir.js'
 
 import ora from 'ora' //加载
 import chalk from 'chalk' //彩色终端文字
+import inquirer from 'inquirer' //创建项目时,回答问题
 
 // 引入init文件=>在本地创建一个项目文件夹=>将文件直接拷贝到文件夹中
 // yyy dev=>本地开发命令=>运行webpack
@@ -22,17 +23,35 @@ program
   .command('init <app-name>')
   .description('使用 white-box-cli 初始化项目')
   .action((name) => {
-    // console.log(chalk.blue('app-name: '), name)
-    const spinner = ora('Loading unicorns').start()
-
-    // 从参数中解析项目名称
-    let tar_path = './' + name
-    copyDir('./template/vue_2.0', tar_path).then(() => {
-      // console.log('加载完成~')
-      spinner.stop()
-    })
-
-    // 根据项目名称创建文件夹
+    //TODO:输入该命令后,进行问题回答
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          message: '请选择框架及框架版本',
+          name: 'frame',
+          default: 'vue2.x',
+          choices: ['vue2.x', 'vue3.x'],
+        },
+      ])
+      .then((answers) => {
+        // 根据选择结果进行框架搭建
+        console.log(chalk.blue('answers'), answers)
+        let tar_path = './' + name
+        let { frame } = answers
+        if (frame == 'vue2.x') {
+          copyDir('./template/vue_2.0', tar_path)
+        } else {
+          copyDir('./template/vue_3.0', tar_path)
+        }
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+      })
   })
 
 //...
