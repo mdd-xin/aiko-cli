5c2b6681221eb6577541781e297d014b680d2c0f
newMdd <3011878963@qq.com>
2023-01-10 00:11:13 +0800

    init

--- template/vue_3.0/src/App.vue
+++ template/vue_3.0/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-}
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <style>
