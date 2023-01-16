8806de1e4d35837ddc7cc6ef3b1412fc485ea93f
newMdd <3011878963@qq.com>
2023-01-16 17:45:24 +0800

    调成

--- src/utils/copyDir.js
+++ src/utils/copyDir.js
@@ -4,25 +4,11 @@
  * @param frame 选择模板
  * */
 import fs from 'fs'
-import axios from 'axios'
 import download from 'download-git-repo'
 
 // 创建文件夹
 export async function copyDir(proName, frame) {
   // 判断文件夹是否存在,存在则报错,不存再进行创建
-  // await mkdirp(target)
-  // let tar_dir = await readDir(source)
-  // for (let item of tar_dir.data) {
-  //   let path = source + '\\' + item
-  //   let { data: isdir } = await isDir(path)
-  //   let dir_child_path = target + '\\' + item
-  //   if (isdir) {
-  //     copyDir(path, dir_child_path)
-  //   } else {
-  //     let s_file = await readFile(path)
-  //     let res = await writeFile(dir_child_path, s_file.data)
-  //   }
-  // }
   /**
    * 改变模板架构
    *
@@ -30,24 +16,8 @@ export async function copyDir(proName, frame) {
    * 1.根据选项拉取对应模板
    * 2.根据拉取后的文件夹内容判断是否拉取成功
    * */
-  axios
-    .get('https://api.github.com/repos/mdd-xin/codeTem/contents')
-    .then((res) => {
-      res.data.forEach((item) => {
-        console.log('item', item.name, frame)
-        if (item.name == frame) {
-          let clone_url = `https://github.com/mdd-xin/codeTem/${frame}`
-          console.log(clone_url)
-          download(
-            'direct:' + clone_url,
-            proName,
-            { clone: true },
-            function (err) {
-              console.log(err ? 'Error' : 'Success')
-              //
-            }
-          )
-        }
-      })
-    })
+  // let clone_url = `https://github.com/mdd-xin/codeTem/${frame}`
+  download('direct:' + clone_url, proName, { clone: true }, function (err) {
+    console.log(err ? 'Error' : 'Success')
+  })
 }
--- src/utils/getTem.js
+++ src/utils/getTem.js
@@ -10,17 +10,20 @@ import axios from 'axios'
 
 axios.get('https://api.github.com/users/mdd-xin/repos').then((res) => {
   res.data.forEach((item) => {
-    // console.log(item.name)
+    console.log(item.clone_url)
     if (item.name == 'vue-suduko') {
       download(
-        'direct:' + 'https://github.com/mdd-xin/codeTem/vue_2.0',
+        'direct:' + item.clone_url,
         'mddTest',
         { clone: true },
         function (err) {
-          console.log(err ? 'Error' : 'Success')
-          //
+          console.log(err ? err : 'Success')
         }
       )
     }
   })
 })
+// const url = 'direct:https://github.com/mdd-xin/codeTem.git#master'
+// download(url, 'mddTest', function (err) {
+//   console.log(err ? err : 'Success')
+// })
