8236eec843fa59b37b36b27ffff8aa3a5706b2e8
newMdd <3011878963@qq.com>
2023-01-18 00:48:34 +0800

    修改测试模板

--- src/utils/getTem.js
+++ src/utils/getTem.js
@@ -8,22 +8,23 @@ import axios from 'axios'
 //   }
 // )
 
-axios.get('https://api.github.com/users/mdd-xin/repos').then((res) => {
-  res.data.forEach((item) => {
-    console.log(item.clone_url)
-    if (item.name == 'vue-suduko') {
-      download(
-        'direct:' + item.clone_url,
-        'mddTest',
-        { clone: true },
-        function (err) {
-          console.log(err ? err : 'Success')
-        }
-      )
-    }
-  })
-})
-// const url = 'direct:https://github.com/mdd-xin/codeTem.git#master'
-// download(url, 'mddTest', function (err) {
-//   console.log(err ? err : 'Success')
+// axios.get('https://api.github.com/users/mdd-xin/repos').then((res) => {
+//   res.data.forEach((item) => {
+//     console.log(item.clone_url)
+//     if (item.name == 'vue-suduko') {
+//       download(
+//         'direct:' + item.clone_url,
+//         'mddTest',
+//         { clone: true },
+//         function (err) {
+//           console.log(err ? err : 'Success')
+//         }
+//       )
+//     }
+//   })
 // })
+
+const url = 'direct:https://github.com/mdd-xin/codeTem.git#tem-vue3'
+download(url, 'mddTest', { clone: true }, function (err) {
+  console.log(err ? err : 'Success')
+})
